e3fec707562440c89f64d89fcb6995603446d509
Tomber0 <aleseyzaicev@gmail.com>
2019-11-01 16:22:33 +0300

    homework_4_complex_numbers

--- pvt_homework_4/ComplexNumber.cs
+++ pvt_homework_4/ComplexNumber.cs
@@ -11,35 +11,29 @@ namespace pvt_homework_4
 
     class ComplexNumber
     {
-        double realPart;
-        double imaginaryPart;
-        ComplexNumber(double newRealPart, double newImaginaryPart) {
+        private double realPart;
+        private double imaginaryPart;
+        public double GetImaginaryPart()
+        {
+            return this.imaginaryPart;
+        }
+        public double GetRealPart()
+        {
+            return this.realPart;
+        }
+
+        public ComplexNumber(double newRealPart, double newImaginaryPart) {
             this.realPart = newRealPart;
             this.imaginaryPart = newImaginaryPart;
         }
-        ComplexNumber(int newRealPart, int newImaginaryPart)
+        public  ComplexNumber(int newRealPart, int newImaginaryPart)
         {
             this.realPart = newRealPart;
             this.imaginaryPart = newImaginaryPart;
-        }
-        public override string ToString()
-        {
-            string complexNumber;
-            if (imaginaryPart <= 0)
-            {
-                if(imaginaryPart < 0)
-                complexNumber = Convert.ToString(this.realPart) + " " + Convert.ToString(this.imaginaryPart) + " * i";
-                else
-                    complexNumber = Convert.ToString(this.realPart);
-
-            }
-            else{
-
-                complexNumber = Convert.ToString(this.realPart) + " + " + Convert.ToString(this.imaginaryPart) + " * i";
-            }
-            return complexNumber;
         }
         static public ComplexNumber SumComplexNumbers(ComplexNumber complexNumberFirst,ComplexNumber complexNumberSecond) {
+            //сумма комплексных числел
+
             double newImagPart;
             double newRealPart;
             newImagPart = complexNumberFirst.imaginaryPart + complexNumberSecond.imaginaryPart;
@@ -50,6 +44,8 @@ namespace pvt_homework_4
         }
         static public ComplexNumber SubstractComplexNumbers(ComplexNumber complexNumberFirst, ComplexNumber complexNumberSecond)
         {
+            //разность комплексных числел
+
             double newImagPart;
             double newRealPart;
             newImagPart = complexNumberFirst.imaginaryPart - complexNumberSecond.imaginaryPart;
@@ -60,15 +56,17 @@ namespace pvt_homework_4
         }
         static public ComplexNumber MutiplicationComplexNumbers(ComplexNumber complexNumberFirst, ComplexNumber complexNumberSecond)
         {
+            //произведение комплексных числел
+
             double newImagPart;
             double newRealPart;
             //x+y*i = Z; u+v*i = C
             //рез выпажение состоит из 2 частей: realpart = .(x*u - y*v) + ImgPart = .(x*v + y*u)*i
 
-            newRealPart = complexNumberFirst.realPart * complexNumberSecond.realPart - 
+            newRealPart = complexNumberFirst.realPart * complexNumberSecond.realPart -
                             complexNumberFirst.imaginaryPart * complexNumberSecond.imaginaryPart;
             newImagPart = complexNumberFirst.realPart * complexNumberSecond.imaginaryPart +
-                            complexNumberFirst.realPart * complexNumberSecond.imaginaryPart;
+                            complexNumberFirst.imaginaryPart * complexNumberSecond.realPart;
 
             ComplexNumber resultOfMutiplicationComplexNumber = new ComplexNumber(newRealPart, newImagPart);
             return resultOfMutiplicationComplexNumber;
@@ -76,27 +74,119 @@ namespace pvt_homework_4
         }
         static public ComplexNumber DivisionComplexNumbers(ComplexNumber complexNumberNumerator, ComplexNumber complexNumberDenominator)
         {
-
-            //деление: 1/(x^2 + y^2) *  (операция умножения:)((u*x + v*y) + (v*x - u*)*i)
-            //
+            //частное комплексных числел
             double newImagPart;
             double newRealPart;
 
-            double newDenominatorPart ;
-            ComplexNumber newInterimMuliplResult = ComplexNumber.MutiplicationComplexNumbers(complexNumberNumerator, complexNumberDenominator);
-            //создание промежуточного компексного числа - являющегося результатом множения 2 данных числел
+
+            double newDenominatorPart;
 
             //x+y*i = Z; u+v*i = C
-            //рез выпажение состоит из 2 частей: realpart = .(x*u - y*v) + ImgPart = .(x*v + y*u)*i
+            //рез выпажение состоит из 2 частей: realpart = .(x*u + y*v) + ImgPart = .(y*u - x*v)*i
 
-            newDenominatorPart = (Math.Pow(complexNumberNumerator.realPart, 2) + Math.Pow(complexNumberNumerator.imaginaryPart, 2));
-            double resultRealPart = newInterimMuliplResult.realPart / newDenominatorPart;
-            double resultImagPart = newInterimMuliplResult.imaginaryPart / newDenominatorPart;
+            newDenominatorPart = (Math.Pow(complexNumberDenominator.realPart, 2) + Math.Pow(complexNumberDenominator.imaginaryPart, 2));
 
-            ComplexNumber resultOfDivisionComplexNumber = new ComplexNumber(resultRealPart, resultImagPart);
+            newRealPart = (complexNumberNumerator.realPart * complexNumberDenominator.realPart +
+                           complexNumberNumerator.imaginaryPart * complexNumberDenominator.imaginaryPart) / newDenominatorPart;
+            newImagPart = (complexNumberNumerator.imaginaryPart * complexNumberDenominator.realPart -
+                complexNumberNumerator.realPart * complexNumberDenominator.imaginaryPart) / newDenominatorPart;
+
+            ComplexNumber resultOfDivisionComplexNumber = new ComplexNumber(newImagPart, newImagPart);
 
             return resultOfDivisionComplexNumber;
 
         }
+
+        public ComplexNumber SumComplexNumber(ComplexNumber complexNumberToSum)
+        {
+            //сумма комплексных числел
+
+            double newImagPart;
+            double newRealPart;
+            newImagPart = this.imaginaryPart + complexNumberToSum.imaginaryPart;
+            newRealPart = this.realPart + complexNumberToSum.realPart;
+            ComplexNumber resultOfSumComplexNumber = new ComplexNumber(newRealPart, newImagPart);
+            return resultOfSumComplexNumber;
+
+        }
+        public ComplexNumber SubstractComplexNumber(ComplexNumber complexNumberSecond)
+        {
+            //разность комплексных числел
+
+            double newImagPart;
+            double newRealPart;
+            newImagPart = this.imaginaryPart - complexNumberSecond.imaginaryPart;
+            newRealPart = this.realPart - complexNumberSecond.realPart;
+            ComplexNumber resultOfSubstrationComplexNumber = new ComplexNumber(newRealPart, newImagPart);
+            return resultOfSubstrationComplexNumber;
+
+        }
+        public ComplexNumber MutiplicationComplexNumbers(ComplexNumber complexNumberMult)
+        {
+            //произведение комплексных числел
+
+            double newImagPart;
+            double newRealPart;
+            //x+y*i = Z; u+v*i = C
+            //рез выпажение состоит из 2 частей: realpart = .(x*u - y*v) + ImgPart = .(x*v + y*u)*i
+
+            newRealPart = this.realPart * complexNumberMult.realPart -
+                            this.imaginaryPart * complexNumberMult.imaginaryPart;
+            newImagPart = this.realPart * complexNumberMult.imaginaryPart +
+                            this.imaginaryPart * complexNumberMult.realPart;
+
+            ComplexNumber resultOfMutiplicationComplexNumber = new ComplexNumber(newRealPart, newImagPart);
+            return resultOfMutiplicationComplexNumber;
+
+        }
+        public ComplexNumber DivisionComplexNumbers(ComplexNumber complexNumberDenominator)
+        {
+            //частное комплексных числел
+
+            //деление: 1/(x^2 + y^2) *  (операция умножения:)((u*x + v*y) + (v*x - u*)*i)
+            //
+            double newImagPart;
+
+            double newRealPart;
+           
+
+           double newDenominatorPart;
+           //x+y*i = Z; u+v*i = C
+           //рез выпажение состоит из 2 частей: realpart = .(x*u + y*v) + ImgPart = .(y*u - x*v)*i
+           
+           newDenominatorPart = (Math.Pow(complexNumberDenominator.realPart, 2) + Math.Pow(complexNumberDenominator.imaginaryPart, 2));
+
+            newImagPart = (this.realPart * complexNumberDenominator.realPart +
+                           this.imaginaryPart * complexNumberDenominator.imaginaryPart) / newDenominatorPart;
+            newImagPart = (this.imaginaryPart * complexNumberDenominator.realPart - 
+                this.realPart * complexNumberDenominator.imaginaryPart) / newDenominatorPart;
+           
+           ComplexNumber resultOfDivisionComplexNumber = new ComplexNumber(newImagPart, newImagPart);
+           
+            return resultOfDivisionComplexNumber;
+
+        }
+        public override string ToString()
+        {
+            //вывод числа 
+            string complexNumber;
+            if (imaginaryPart <= 0)
+            {
+                if (imaginaryPart < 0)
+                    complexNumber = Convert.ToString(this.realPart) + " " + Convert.ToString(this.imaginaryPart) + " * i";
+                else
+                    complexNumber = Convert.ToString(this.realPart);
+
+            }
+            else
+            {
+
+                complexNumber = Convert.ToString(this.realPart) + " + " + Convert.ToString(this.imaginaryPart) + " * i";
+            }
+            return complexNumber;
+        }
+
+
+
     }
 }
--- pvt_homework_4/Program.cs
+++ pvt_homework_4/Program.cs
@@ -10,6 +10,29 @@ namespace pvt_homework_4
     {
         static void Main(string[] args)
         {
+            ComplexNumber complexNumber1 = new ComplexNumber(1, 5);
+
+            ComplexNumber complexNumber2 = new ComplexNumber(3, -3);
+            Console.Out.WriteLine(complexNumber1.ToString());
+            Console.Out.WriteLine(complexNumber2.ToString());
+            Console.Out.WriteLine("<++++>");
+
+            
+            ComplexNumber sumOfNums =
+                complexNumber1.SumComplexNumber(complexNumber2);
+            Console.Out.WriteLine("Сумма = {0}",sumOfNums.ToString());
+            ComplexNumber difOfNums =
+                complexNumber1.SubstractComplexNumber(complexNumber2);
+            Console.Out.WriteLine("Разность = {0}", difOfNums.ToString());
+            ComplexNumber multOfNums =
+                complexNumber1.MutiplicationComplexNumbers(complexNumber2);
+            Console.Out.WriteLine("Произведение = {0}", multOfNums.ToString());
+            ComplexNumber divOfNums =
+                complexNumber1.DivisionComplexNumbers(complexNumber2);
+            Console.Out.WriteLine("Деление = {0}", divOfNums.ToString());
+
+
+
         }
     }
 }
